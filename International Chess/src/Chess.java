cf5c6092517fe89eaeda6af3a7998f5e7dd5c6c5
Luciferhaha <hahahaEden@163.com>
2017-03-29 15:56:47 +0800

    Version 1.0

--- International Chess/src/Chess.java
+++ International Chess/src/Chess.java
@@ -1,23 +1,28 @@
 import java.awt.Label;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 //处理不同棋子的算法
 //兵，后，王，车，象，马
-//兵变身，王车短易位和长易位
+//兵变身，兵吃过路兵，王车短易位和长易位
+//行棋权 白棋先走，双方轮流走棋
+//一着棋，除参与易位的车以外，任何棋子都不能越过被其他棋子占据的格子
+//一旦吃掉，必须从棋盘上拿走
 //让不同类比的棋子各司其职（执行各自的方法）
-public class Chess {
+public class Chess implements MouseListener{
 	public Label[][] chess;
 	public Chess() {
 		// TODO Auto-generated constructor stub
 		//总共32个棋子
-		chess =new Label[3][7];
+		chess =new Label[4][8];
 		JobDivision();
 	}
 	//职责划分
 	public void JobDivision() {
 		//两列士兵8*2
 		for (int i = 0; i < 8; i++) {
-			Soilder(chess[1][i]);
-			Soilder(chess[2][i]);
+			Pawn(chess[1][i]);
+			Pawn(chess[2][i]);
 		}
 		//双王
 		King(chess[0][4]);
@@ -31,10 +36,10 @@ public class Chess {
 		Rook(chess[3][0]);
 		Rook(chess[3][7]);
 		//四象
-		BIsshop(chess[0][2]);
-		BIsshop(chess[0][5]);
-		BIsshop(chess[3][2]);
-		BIsshop(chess[3][5]);
+		Bisshop(chess[0][2]);
+		Bisshop(chess[0][5]);
+		Bisshop(chess[3][2]);
+		Bisshop(chess[3][5]);
 		//四马
 		Knight(chess[0][1]);
 		Knight(chess[0][6]);
@@ -43,22 +48,69 @@ public class Chess {
 		
 		
 	}
-	public void Soilder(Label chess){
-		
+	public void Pawn(Label chess){
+	//其第一步可以向前走一或两格，以后每次只可向前走一步，不可往后走
+	//吃对方的棋子则是向前打斜来吃
+	
 	} 
 	public void Queen(Label chess) {
-		
+		//上下左右，四个方向斜
 	}
 	public void King(Label chess) {
-		
+		//上下左右斜每次移动一步
 	}
 	public void Rook(Label chess) {
-		
+		//上下左右
 	}
 	public void Knight(Label chess){
+		//日子格
+	}
+	public void Bisshop(Label chess) {
+		//上下左右斜
+	}
+	//兵的升变
+	public void promotion() {
+		//当兵子走至对方底线，玩家可选择把该兵升级成车，马，象或后。
+	}
+	public void En_Passant() {
+		//吃路过兵
+		//1.对方的兵必须是在原位第一次移动且直进两格
+		//2.形成本方有兵与其横向紧贴并列
+		//3.吃的方式为斜进，并拿掉对方棋子
+	}
+	public void Castling() {
+		//王车长短易位
+		//1.王车从来没有移位
+		//2.王和车之间没有棋子
+		//3.王的原始格子或者将要越过的格子或者将要占据的格子正受到对方棋子的攻击
+		//王往右两个或者往左两个，车换到王的里侧
+	}
+	public void Removechess(){
 		
 	}
-	public void BIsshop(Label chess) {
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
 	
--- International Chess/src/ChessBoard.java
+++ International Chess/src/ChessBoard.java
@@ -9,7 +9,7 @@ public class ChessBoard  extends JPanel{
 	public final static int width=800;
 	public final static int height=800;
 	public final static int gap=50;//实际棋盘和边框的间距
-	public  final static int side=100;//小正方形的边框
+	public  final static int side=85;//小正方形的边框
 	//位置参数
 	public  Location[][] grid;
 	//棋子的对象
@@ -19,7 +19,7 @@ public class ChessBoard  extends JPanel{
 	public ChessBoard() {
 		// TODO Auto-generated constructor stub
 		//初始化坐标和棋子，行列宽
-		grid=new Location [7][7];
+		grid=new Location [8][8];
 		pieces=new Chess();
 		row=column=8;
 	}
@@ -29,38 +29,38 @@ public class ChessBoard  extends JPanel{
 		//两行士兵的初始位置
 		for (int i = 0; i <column; i++) {
 			for (int j = 0; j <column; j++) {
-				pieces.chess[1][i].setLocation(j*100, 100);
-				pieces.chess[3][i].setLocation(j*100, 100);
+				pieces.chess[1][i].setLocation(j*side, side);
+				pieces.chess[3][i].setLocation(j*side, side);
 				
 			}
 		}
 		//双王的初始位置
-		pieces.chess[0][4].setLocation(400, 700);
-		pieces.chess[3][4].setLocation(400,0);
+		pieces.chess[0][4].setLocation(4*side, 7*side);
+		pieces.chess[3][4].setLocation(4*side,0);
 		//双后的初始位置
-		pieces.chess[0][3].setLocation(300, 700);
-		pieces.chess[3][3].setLocation(300,0);
+		pieces.chess[0][3].setLocation(3*side, 7*side);
+		pieces.chess[3][3].setLocation(3*side,0);
 		//四车的初始位置
-		pieces.chess[0][0].setLocation(0,700);
-		pieces.chess[0][7].setLocation(700,700);
+		pieces.chess[0][0].setLocation(0,7*side);
+		pieces.chess[0][7].setLocation(7*side,7*side);
 		pieces.chess[3][0].setLocation(0,0);
-		pieces.chess[3][7].setLocation(0,700);
+		pieces.chess[3][7].setLocation(0,7*side);
 		//四象的初始位置
-		pieces.chess[0][2].setLocation(200, 700);
-		pieces.chess[0][5].setLocation(500, 700);
-		pieces.chess[3][2].setLocation(200, 0);
-		pieces.chess[3][5].setLocation(500, 0);
+		pieces.chess[0][2].setLocation(2*side, 7*side);
+		pieces.chess[0][5].setLocation(5*side, 7*side);
+		pieces.chess[3][2].setLocation(2*side, 0);
+		pieces.chess[3][5].setLocation(5*side, 0);
 		//四马的初始位置
-		pieces.chess[0][1].setLocation(100, 700);
-		pieces.chess[0][6].setLocation(600, 700);
-		pieces.chess[3][1].setLocation(100, 0);
-		pieces.chess[3][6].setLocation(600, 0);
+		pieces.chess[0][1].setLocation(1*side, 7*side);
+		pieces.chess[0][6].setLocation(6*side, 7*side);
+		pieces.chess[3][1].setLocation(1*side, 0);
+		pieces.chess[3][6].setLocation(6*side, 0);
 	}
 				
 	public  void paint(Graphics g) {
 		super.paint(g);
 		g.setColor(Color.white);
-		g.fillRect(gap, 0,width ,height);
+		g.fillRect(gap, 0,row*side,column*side);
 		for (int i = 0; i < grid.length; i=i+1) {//row
 			for (int j = 0; j < grid.length; j=j+2) {//column
 				g.setColor(Color.black);
