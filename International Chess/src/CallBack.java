97af6579c170346f866acc0116f7dd8a23fe5dfe
Luciferhaha <hahahaEden@163.com>
2017-05-03 16:31:55 +0800

    some changes

---
+++ International Chess/src/CallBack.java
@@ -0,0 +1,4 @@
+
+public interface CallBack {
+	public void deliver();
+}
---
+++ International Chess/src/pieces.java
@@ -0,0 +1,47 @@
+import javax.swing.JLabel;
+
+public class pieces  extends JLabel{
+	public pieces() {
+		// TODO Auto-generated constructor stub
+		
+		
+	}
+	//white pieces
+	public void name() {
+		
+	}
+	public void WPieces() {
+		
+	}
+	public void WQueen() {
+		
+	}
+	public void WKIng() {
+		
+	}
+	public void WBishop() {
+		
+	}
+	public void WKnight() {
+		
+	}
+	public void WRook() {
+		
+	}
+
+	public void BQueen() {
+		
+	}
+	public void BKIng() {
+		
+	}
+	public void BBishop() {
+		
+	}
+	public void BKnight() {
+		
+	}
+	public void BRook() {
+		
+	}
+}
