3292dbbce3d74008b168da509ca9f5e91049cac2
AndrewBDIC <895011839@QQ.COM>
2017-05-16 22:00:35 +0800

    Add files via upload

---
+++ NewGUI/src/Login.java
@@ -0,0 +1,85 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Login implements ActionListener{
+
+	JFrame mainJFrame;
+	Container con;
+	JLabel labName,labPass,labID,labTitle,labEmpty;
+	JTextField txtName,txtID;
+	JPasswordField txtPass;
+	JButton login1,register,cancel;
+	
+
+public Login(){
+	mainJFrame=new JFrame("User log in");
+	con=mainJFrame.getContentPane();
+	con.setLayout(new FlowLayout());
+	labTitle=new JLabel("<html><body><h1> Welcome to Chess Game ! \n <br> </h1> </body>  </html>");
+	
+	labID=new JLabel("Your email address:");
+	txtID=new JTextField();
+	txtID.setColumns(20);
+	
+	labName=new JLabel("  User name:");
+	txtName=new JTextField();
+	txtName.setColumns(20);
+	
+	labPass=new JLabel("   password:");
+	txtPass=new JPasswordField();
+	txtPass.setColumns(20);
+	
+	login1=new JButton("Log in");
+	login1.addActionListener(this);
+	
+	register=new JButton("Register");
+	register.addActionListener(this);
+	
+	cancel=new JButton("Cancel");
+	cancel.addActionListener(this);
+	
+	
+	con.add(labTitle);
+	con.add(Box.createHorizontalStrut(30000));//”√¿¥ªª––
+	
+	con.add(labID);
+	con.add(txtID);
+	con.add(Box.createHorizontalStrut(30000));
+	
+	con.add(labName);
+	con.add(txtName);
+	con.add(Box.createHorizontalStrut(30000));
+	
+	con.add(labPass);
+	con.add(txtPass);
+	con.add(Box.createHorizontalStrut(30000));
+	
+	con.add(login1);
+	con.add(register);
+	con.add(cancel);
+	
+	mainJFrame.setSize(350, 350);
+	mainJFrame.setVisible(true);
+	mainJFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
+public void actionPerformed(ActionEvent e){
+	if(e.getSource()==login1){
+		
+	}
+	if(e.getSource()==register){
+		mainJFrame.setVisible(false);
+		Registry reg=new Registry();
+		reg.mainJFrame.setVisible(true);
+	}
+	if(e.getSource()==cancel){
+		txtID.setText(null);
+		txtName.setText(null);
+		txtPass.setText(null);
+		mainJFrame.setVisible(false);
+	}
+}
+public static void main(String[] args){
+	new Login();
+  }
+}
---
+++ NewGUI/src/Registry.java
@@ -0,0 +1,115 @@
+
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class Registry implements ActionListener {
+
+	JFrame mainJFrame;
+	Container con;
+	JLabel labName,labPass,labRPass,labTitle,labEmpty1,labEmpty,labSex,labAge,labClass;
+	JTextField txtName,txtID,txtAge;
+	JPasswordField txtPass,txtRPass;
+	JButton resetBtn,regBtn,cancel;
+	JRadioButton mRadio,fRadio;
+	ButtonGroup sexBtnGroup;
+	static final String msg1[]={"153721","153722","I do not want to tell you"};
+	JComboBox Jcombobox1;
+	
+public Registry(){
+	mainJFrame=new JFrame("Register");
+	con=mainJFrame.getContentPane();
+	con.setLayout(new FlowLayout());
+	
+	labTitle=new JLabel("<html><body><h1>User register     </h1> </body>  </html> ");
+	con.add(labTitle);
+	con.add(Box.createHorizontalStrut(30000));//用来换行
+	
+	labName=new JLabel("Choose a name");
+	txtName=new JTextField();
+	txtName.setColumns(20);
+	con.add(labName);
+	con.add(txtName);
+	con.add(Box.createHorizontalStrut(30000));//用来换行
+	
+	labPass=new JLabel("New password:");
+	txtPass=new JPasswordField();
+	txtPass.setColumns(20);
+	con.add(labPass);
+	con.add(txtPass);
+	con.add(Box.createHorizontalStrut(30000));//用来换行
+	
+	labRPass=new JLabel("Confirm your password:");
+	txtRPass=new JPasswordField();
+	txtRPass.setColumns(20);
+	con.add(labRPass);
+	con.add(txtRPass);
+	con.add(Box.createHorizontalStrut(30000));//用来换行
+	
+	 
+	labSex=new JLabel("Gender:");// 这里用两个按钮来处理
+	mRadio=new JRadioButton("I am a boy",true);
+	mRadio.addActionListener(this);
+	fRadio=new JRadioButton("I am a lady",false);
+	fRadio.addActionListener(this);
+	sexBtnGroup=new ButtonGroup();
+	sexBtnGroup.add(mRadio);
+	sexBtnGroup.add(fRadio);
+	con.add(labSex);
+	con.add(mRadio);
+	con.add(fRadio);
+	
+	labAge=new JLabel("Age:");
+	txtAge=new JTextField();
+	txtAge.setColumns(5);
+	con.add(labAge);
+	con.add(txtAge);
+	con.add(Box.createHorizontalStrut(30000));//用来换行
+	
+	labClass=new JLabel("Your class:");//这里使用下拉选项框来显示选项
+	Jcombobox1=new JComboBox(msg1);
+	//Jcombobox1.addItemListener(this);
+	con.add(labClass);
+	con.add(Jcombobox1);
+	con.add(Box.createHorizontalStrut(30000));//用来换行
+	
+	regBtn=new JButton("Register");
+	regBtn.addActionListener(this); 
+	con.add(regBtn);
+	
+	resetBtn=new JButton("Reset");
+	resetBtn.addActionListener(this);
+	con.add(resetBtn);
+	
+	cancel=new JButton("Cancel");
+	cancel.addActionListener(this);
+	con.add(cancel);
+	
+	mainJFrame.setSize(400,400);
+	mainJFrame.setVisible(true);
+	mainJFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
+public void actionPerformed(ActionEvent e){
+	if(e.getSource()==regBtn){
+		
+	}
+	if(e.getSource()==resetBtn){
+		txtName.setText(null);
+		txtPass.setText(null);
+		txtRPass.setText(null);
+		mRadio.setSelected(true);
+		txtAge.setText(null);
+		Jcombobox1.setSelectedIndex(0);
+		}
+	if(e.getSource()==cancel){
+		mainJFrame.setVisible(false);
+		Login login=new Login();
+		login.mainJFrame.setVisible(true);
+	}
+}
+public static void main(String[] args){
+	new Registry();
+}
+}
+
